82a767b99c004eb74b3a7ff2eb8d48d41b249f2d
aaronksaunders <aaron@clearlyinnovative.com>
2018-10-28 18:15:36 -0400

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Apollo } from 'apollo-angular';
+import gql from 'graphql-tag';
+import { ApolloQueryResult } from 'apollo-client';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  rates: any[];
+  loading = true;
+  error: any;
 
+  constructor(private apollo: Apollo) {}
+
+  ngOnInit() {
+    this.apollo
+      .watchQuery({
+
+        // THIS IS THE QUERY...
+        query: gql`
+          {
+            rates(currency: "USD") {
+              currency
+              rate
+            }
+          }
+        `,
+      })
+      // WHEN THE VALUE CHANGES, WE GET A STREAM FROM THE SUBSCRIPTION
+      .valueChanges.subscribe((result : ApolloQueryResult<any> ) => {
+
+        // THE RATES THAT ARE RETURNED ARE NOW AVAILABLE
+        // FOR DISPLAY IN THE UI
+        this.rates = result.data && result.data.rates;
+
+        // LOADING STATUS
+        this.loading = result.loading;
+
+        // ERRORS
+        this.error = result.errors;
+      });
+  }
 }
